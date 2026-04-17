66508feb827d0e895cf805a8dbdfbc17df04088c
Estevão Jung <jungestevao25@gmail.com>
2026-04-17 16:00:07 -0300

    Atividade Prática - JavaScript básico - matrícula: 91050

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 let nome = ""
 let renda = ""
-let despesas = ""
+let qdespesas = ""
 
 while (!nome || !isNaN(nome)) {
     nome = prompt("Qual seu nome?: ");
@@ -14,33 +14,34 @@ while (renda === "" || isNaN(Number(renda))) {
         alert("Por favor, digite um valor (apenas números). ");
     }
 }
-while (despesas === "" || isNaN(Number(despesas))) {
-    despesas = prompt("Digite a sua quantidade de despesa: ");
-    if (despesas === "" || isNaN(Number(despesas))) {
+while (qdespesas === "" || isNaN(Number(qdespesas))) {
+    qdespesas = prompt("Digite a sua quantidade de despesa: ");
+    if (qdespesas === "" || isNaN(Number(qdespesas))) {
         alert("Por favor, digite um valor (apenas números). ");
     }
 }
-despesas = Number(despesas)
-if (despesas >= 5) {
-    despesas = 5
-}else if (despesas <= 1) {
-    despesas = 1
+qdespesas = Number(qdespesas)
+if (qdespesas >= 5) {
+    qdespesas = 5
+}else if (qdespesas <= 1) {
+    qdespesas = 1
 }
-console.log(`A quantidade de despesas é ${despesas}`)
-let limite = despesas;
-despesas = 0
+console.log(`A quantidade de despesas é ${qdespesas}`)
+let limite = qdespesas;
+let despesas = 0
 for (let i = 1; i <= limite; i++) {
     despesas += Number(prompt(`Digite o valor da despesa ${i}: `))
 }
 
-console.log(nome)
-console.log(Number(renda))
-console.log(despesas)
+console.log(`Nome de Usuário: ${nome}`)
+console.log(`Renda: R$${Number(renda).toFixed(2)}`)
+console.log(`Total de Despesas: R$${Number(despesas).toFixed(2)} `)
 
 if (despesas > renda){
     console.log("⚠️ Atenção: você gastou mais do que ganhou.")
 } else {
     let sobra = (renda - despesas);
+    console.log(`Sobra: R$${Number(sobra).toFixed(2)}`)
     let psobra = ((sobra / renda) * 100);
     if (psobra >= 30) {
         console.log(`✅ Ótimo: boa margem de sobra.`);
